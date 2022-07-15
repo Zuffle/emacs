524d6e1a1140c9a4f2f6c88b13d523391d612fb9
Justin Parry <jep@Justins-MBP.lan>
2022-07-15 11:01:54 -0500

    more keybinds and ivy/hydra

--- init.el
+++ init.el
@@ -161,14 +161,38 @@
 (use-package howm
   )
 
+(use-package hydra
+  )
+
+(use-package ivy-hydra)
+
+;;;;;;;;;;;;;;;;;;
+;; Hydra Macros ;;
+;;;;;;;;;;;;;;;;;;
+
+;; text scale hydra
+(defhydra hydra-text-scale (:timeout 4)
+  "scale text"
+  ("j" text-scale-decrease "out")
+  ("k" text-scale-increase "in")
+  ("0" (text-scale-set 0))
+  ("q" nil "quit")
+  )
+  
+;; buffer hydra
+(defhydra hydra-buffer-nav (:timeout 4)
+  "navigate buffers"
+)
+
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Key Bindings		  	     ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; (general-def
-;;   :prefix "SPC"
-;;   "" nil
-;;   ";;" '(comment-line :which-key "comment line"))
+(general-def
+  "C-g" nil
+  "C-g" 'evil-escape)
+
 
 
 
@@ -179,13 +203,47 @@
   )
 
 (jep/leader-keys
+  "TAB" '(spacemacs/alternate-buffer :wk "last buffer")
+
   ;; prefix key setup
   "f" '(:ignore t :wk "files")
   "t" '(:ignore t :wk "toggles")
   "a" '(:ignore t :wk "applications")
   "w" '(:ignore t :wk "window")
+  "h" '(:ignore t :wk "help")
 
+  ;; HELP
+  "hdv" '(describe-variable :wk "describe variable")
+  "hdf" '(describe-function :wk "describe function")
+  "hdk" '(describe-key :wk "describe key")
 
   "tt" '(counsel-load-theme :which-key "choose theme")
+  "ts" '(hydra-text-scale/body :wk "scale text")
   "ff" '(counsel-find-file :wk "find-file")
+
   )
+
+
+(defun spacemacs/alternate-buffer (&optional window)
+  "Switch back and forth between current and last buffer in the
+current window.
+If `spacemacs-layouts-restrict-spc-tab' is `t' then this only switches between
+the current layouts buffers."
+  (interactive)
+  (cl-destructuring-bind (buf start pos)
+      (if (bound-and-true-p spacemacs-layouts-restrict-spc-tab)
+          (let ((buffer-list (persp-buffer-list))
+                (my-buffer (window-buffer window)))
+            ;; find buffer of the same persp in window
+            (seq-find (lambda (it) ;; predicate
+                        (and (not (eq (car it) my-buffer))
+                             (member (car it) buffer-list)))
+                      (window-prev-buffers)
+                      ;; default if found none
+                      (list nil nil nil)))
+        (or (cl-find (window-buffer window) (window-prev-buffers)
+                     :key #'car :test-not #'eq)
+            (list (other-buffer) nil nil)))
+    (if (not buf)
+        (message "Last buffer not found.")
+      (set-window-buffer-start-and-point window buf start pos))))
