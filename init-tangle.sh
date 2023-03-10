cd5f6e086bc2a6a569556ddb5089849a6b600947
Justin Parry <8173816+Zuffle@users.noreply.github.com>
2023-03-09 21:20:23 -0600

    updated org-babel-tangle-file to reflect variable change

--- init-tangle.sh
+++ init-tangle.sh
@@ -5,10 +5,10 @@
 
 
 (let ((config-file "~/.config/emacs-config/litfig.org")
-      (init-file "~.config/emacs/init.el")
-      (early-init "~.config/emacs/early-init.el")
+      (init-file "~./config/emacs/init.el")
+      (early-init "~./config/emacs/early-init.el")
       )
   ;; (print init-file)
   
-  (org-babel-tangle-file init-file)
+  (org-babel-tangle-file config-file)
   )
