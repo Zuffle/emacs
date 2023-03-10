fc84bfacec82d8da77b769675373c9a1dcbb7862
Justin Parry <8173816+Zuffle@users.noreply.github.com>
2023-03-09 21:45:08 -0600

    added some output and cleaned up unneeded code

--- init-tangle.sh
+++ init-tangle.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 ":"; exec emacs --quick --script "$0" "$@" # -*- mode: emacs-lisp; lexical-binding: t; -*-
 (require 'org)
-;; (require 'org-babel)
-
 
 (let ((config-file "~/.config/emacs-config/litfig.org")
-      (init-file "~./config/emacs/init.el")
-      (early-init "~./config/emacs/early-init.el")
       )
-  ;; (print init-file)
-  
+  (princ (format "Tangling file %s." config-file))
   (org-babel-tangle-file config-file)
   )
