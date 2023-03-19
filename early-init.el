135a01d9a460aeb89717557ed064b24f701ce635
Justin Parry <8173816+Zuffle@users.noreply.github.com>
2023-03-18 21:43:25 -0500

    cleaned up litfig and added Evil Surround. changed undo tree dir

--- early-init.el
+++
@@ -1,3 +0,0 @@
-(setq package-enable-at-startup nil)	; from straight.el to avoid
-                                        ; pacakge.el loading packages
-                                        ; prior to init.el
