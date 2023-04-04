accfe62a12c90203532edc88f8059ea4039536b0
Justin Parry <8173816+Zuffle@users.noreply.github.com>
2023-04-04 17:46:07 -0500

    path to litfig.org in init-tangle.sh is generic now

--- init-tangle.sh
+++ init-tangle.sh
@@ -2,8 +2,8 @@
 ":"; exec emacs --quick --script "$0" "$@" # -*- mode: emacs-lisp; lexical-binding: t; -*-
 (require 'org)
 
-(let ((config-file "~/.config/emacs-config/litfig.org")
+(let ((config-file (concat default-directory "litfig.org"))
       )
-  (princ (format "Tangling file %s.\n" config-file))
+  (princ (format "Tangling snippets from %s.\n" config-file))
   (org-babel-tangle-file config-file)
   )
