9540b37065a4abecbe57d83810d8a3389c4e95b9
Justin Parry <justin.parry@ericsson.com>
2023-04-04 17:31:34 -0500

    made path to litfig.org generic in init-tangle.sh

--- init-tangle.sh
+++ init-tangle.sh
@@ -2,7 +2,10 @@
 ":"; exec emacs --quick --script "$0" "$@" # -*- mode: emacs-lisp; lexical-binding: t; -*-
 (require 'org)
 
-(let ((config-file "~/.config/emacs-config/litfig.org")
+(let ((config-file (concat
+		    (file-name-directory (buffer-file-name))
+		    "listfig.org"
+		    )))
       )
   (princ (format "Tangling file %s.\n" config-file))
   (org-babel-tangle-file config-file)
